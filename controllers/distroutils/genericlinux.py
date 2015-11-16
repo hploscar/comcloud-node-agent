d324c613428b695ab772420747c99df544925d7e
Jose Fuentes Castillo <jsfuentescastillo@gmail.com>
2015-11-16 16:36:47 +0100

    crane installation

--- controllers/distroutils/genericlinux.py
+++ controllers/distroutils/genericlinux.py
@@ -1,11 +1,67 @@
 import subprocess
 
 from tools import files
+import settings
 
 
 def installDocker():
+    # docker engine
     command = 'curl https://get.docker.com/ | bash'+' 1> '+ files.getLogPath('docker') +' 2> '+ files.getLogPath('docker')
     p = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, cwd='/tmp')
+
+    response = ''
+    for line in p.stdout.readlines():
+        response+=line+os.linesep
+
+    if p.wait() == 0:
+        pass
+    else:
+        return False
+
+    # docker compose
+    command = 'curl -L https://github.com/docker/compose/releases/download/1.5.1/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose && chmod +x /usr/local/bin/docker-compose '+' 1> '+ files.getLogPath('docker') +' 2> '+ files.getLogPath('docker')
+    p = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, cwd='/tmp')
+
+    response = ''
+    for line in p.stdout.readlines():
+        response+=line+os.linesep
+
+    if p.wait() == 0:
+        return True
+    else:
+        return False
+
+
+def installCrane():
+    # download
+    command = 'wget -O crane.tar.gz '+settings.CRANE_ORIGIN+' | bash'+' 1> '+ files.getLogPath('crane') +' 2> '+ files.getLogPath('crane')
+    p = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, cwd='/tmp/crane-install')
+
+    response = ''
+    for line in p.stdout.readlines():
+        response+=line+os.linesep
+
+    if p.wait() == 0:
+        pass
+    else:
+        return False
+
+    # extract
+    command = 'tar xzvf crane.tar.gz && mv ./docker-crane-* '+settings.CRANE_INSTALL_FOLDER+' 1> '+ files.getLogPath('crane') +' 2> '+ files.getLogPath('crane')
+    p = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, cwd='/tmp/crane-install')
+
+    response = ''
+    for line in p.stdout.readlines():
+        response+=line+os.linesep
+
+    if p.wait() == 0:
+        pass
+    else:
+        return False
+
+    # execute
+    command = 'docker-compose -d -f production.yml up '+' 1> '+ files.getLogPath('crane') +' 2> '+ files.getLogPath('crane')
+    p = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, cwd=settings.CRANE_INSTALL_FOLDER)
 
     response = ''
     for line in p.stdout.readlines():
--- controllers/installation.py
+++ controllers/installation.py
@@ -1,11 +1,22 @@
 import platform
 from threading import Thread
+import time
+import requests
 
 import settings as s
 import tools
 from tools import files
 from errors import ResourceExistsError, ControllerError, PlatformNotSupportedError, InstallationError
 from distroutils import genericlinux
+
+def _testCrane():
+    try:
+        r = requests.get('http://localhost:8888/extra/alive')
+        if not r:
+            return False
+        return True
+    except:
+        return False
 
 def _installDockerThread():
     try:
@@ -30,19 +41,22 @@ def _installDockerThread():
 
 def _installCraneThread():
     try:
-        # Detect platform
-        distro = platform.linux_distribution()
+        if not genericlinux.installCrane():
+            raise InstallationError('Installation failed. Manual action required.')
+        except:
+            raise ControllerError('Installator execution failure.')
 
-        if distro[0] != '':
-            try:
-                if not genericlinux.installDocker():
-                    raise InstallationError('Installation failed. Manual action required.')
-            except:
-                raise ControllerError('Installator execution failure.')
-        else:
-            raise PlatformNotSupportedError('Could not identify linux distribution')
+        files.writeCraneStatus('installed', 'Crane has been installed and it must be available in some minutes.')
 
-        files.writeDockerStatus('running', 'Docker has been installed and it must be running.')
+        attempts = settings.CRANE_CON_ATT
+        while not _testCrane() and attempts>0:
+            time.sleep(settings.CRANE_TIMEBETWEENATT)
+            attempts = attempts-1
+
+        if attempts == 0:
+            raise InstallationError('Cannot connect with Crane. Maybe a problem ocurred and was not detected.')
+
+        files.writeCraneStatus('running', 'Crane must be running.')
 
     except PlatformNotSupportedError as e:
         files.writeDockerStatus('error', e.message)
