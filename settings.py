6f92876baae4122ae4694dcf40b3158218e01388
Jose Fuentes Castillo <jsfuentescastillo@gmail.com>
2015-11-18 17:37:15 +0100

    changed crane repo

--- settings.py
+++ settings.py
@@ -1,3 +1,4 @@
+
 import os
 GENERAL_DEBUG = (True if os.environ.get('DEBUG')=='true' else False)
 
@@ -9,7 +10,7 @@ WS_BIND_PORT=5050
 APP_DATA_DIR='/var/comcloud-agent'
 
 # Crane
-CRANE_ORIGIN='https://github.com/wtelecom/docker-crane/archive/0.9.5.tar.gz'
+CRANE_ORIGIN='https://github.com/wtelecom/docker-crane/archive/0.9.5-cc.tar.gz'
 CRANE_INSTALL_FOLDER='/usr/src/crane'
 CRANE_CON_ATT=20 # Number of connection attempts while waiting crane to come up
 CRANE_TIMEBETWEENATT=30 # Number of seconds between attempts
